88f54a27b4d102662d474d92ddf99b95ff65e578
Jayson Camba <mjmcamba@gmail.com>
2016-01-17 10:56:38 +0800

    added bootstrap

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '4.2.4'
 #gem 'mysql2', '~> 0.3.10'
 gem 'pg'
 # Use SCSS for stylesheets
+gem 'bootstrap-sass', '~> 3.3.1'
+gem 'autoprefixer-rails'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-  	sc_service = SoundCloudServices::Base.new()
-  	@hot_tracks = sc_service.search("track")
+  	# sc_service = SoundCloudServices::Base.new()
+  	# @hot_tracks = sc_service.search("track")
+  	@hot_tracks = []
   end
 
   def search_results
